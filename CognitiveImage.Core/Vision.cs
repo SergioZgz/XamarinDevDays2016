b647f43ada8685852426af4b28ada86b11700cb7
Sergio Gasca <sgasca@bravent.net>
2016-11-22 10:37:17 +0100

    Android & Core PCL Working

---
+++ CognitiveImage.Core/Vision.cs
@@ -0,0 +1,49 @@
+﻿namespace CognitiveImage.Core
+{
+    #region Usings
+
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.ProjectOxford.Vision;
+    using Plugin.Media;
+    using Plugin.Media.Abstractions;
+
+    #endregion
+
+    public class Vision
+    {
+        private const string VisionKey = "";
+
+        public Vision()
+        {
+            CrossMedia.Current.Initialize();
+        }
+
+        public async Task<string> GetDescription(Stream stream)
+        {
+            VisionServiceClient visionClient = new VisionServiceClient(VisionKey);
+            VisualFeature[] features = { VisualFeature.Tags, VisualFeature.Categories, VisualFeature.Description };
+            stream.Seek(0, SeekOrigin.Begin);
+            var data = await visionClient.AnalyzeImageAsync(stream, features, null);
+
+            return string.Join(", ", data.Description.Captions.Select(p => p.Text));
+        }
+
+        public async Task<MediaFile> TakePhoto()
+        {
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg",
+                    SaveToAlbum = false,
+                    PhotoSize = PhotoSize.Small,
+                    CompressionQuality = 90,
+                    DefaultCamera = CameraDevice.Front
+                });
+            
+            return file;
+        }
+    }
+}
